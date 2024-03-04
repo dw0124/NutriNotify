577685e37f71e7a71b18138d75bcfc51636750c8
dw0124 <81311229+dw0124@users.noreply.github.com>
2024-03-04 17:21:55 +0900

    Initial Commit

---
+++ NutriNotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NutriNotify
+//
+//  Created by 김두원 on 2024/03/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
